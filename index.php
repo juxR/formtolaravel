4d41c7577ed22bb6f07d60c23a1c61cd809ecc79
julienroland <roland.julien.perso@gmail.com>
2014-02-08 13:19:26 +0100

    avancement

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php 
 include_once('dom/simple_html_dom.php');
 $html = new simple_html_dom();
-$value = $_POST['form'];
+$value = $_POST['formToLaravel'];
 /* DATA */
 $html->load($value);
+$totalForm = [];
 
 /* FORM TAG */ 
 $form = $html->find('form')[0];
@@ -12,8 +13,15 @@ $form_url = $form->attr['url'];
 $form_route = $form->attr['route'];
 $form_action = $form->attr['action'];
 $form_param = $form->attr['param'];
+foreach($html->find('form') as $form){
+
+    foreach($form->children as $item){
+        var_dump($item->tag);
+    }
+}
 /* END FORM TAG */ 
-/* LABEL TAGS */
+/* LABEL & INPUT TAGS */
+/* LABEL */
 $labels = $html->find('label');
 
 $label=[];
@@ -28,14 +36,16 @@ for($i=0; $i < count($labels);$i++){
     'for' => $labels[$i]->attr['for'],
     'class' => $labels[$i]->attr['class'],
     'id' => $labels[$i]->attr['id'],
-    'html'=>utf8_decode($labels[$i]->plaintext),
+    'html'=>$labels[$i]->plaintext,
     'type'=>$labels[$i]->tag,
     'data' => $label_data,
     ];
 
 
 }
-$inputs = $html->find('input[type!=submit]');
+/* INPUT */
+$inputs = $html->find('input[type=text],input[type=email],input[type=button],input[type=checkbox],input[type=color],input[type=date],,input[type=datetime],input[type=datetime-local],,input[type=file],input[type=image],input[type=month],input[type=number],input[type=password],input[type=radio],input[type=range],input[type=reset],input[type=search],input[type=tel],input[type=time],input[type=url],input[type=week]');
+
 $input=[];
 $input_data=[];
 for($i=0; $i < count($inputs);$i++){
@@ -45,7 +55,7 @@ for($i=0; $i < count($inputs);$i++){
         }
     }
     $input[$i] = (object)[
-    'type' => $inputs[$i]->tag,
+    'type' => $inputs[$i]->attr['type'],
     'class' => $inputs[$i]->attr['class'],
     'id' => $inputs[$i]->attr['id'],
     'name' => $inputs[$i]->attr['name'],
@@ -56,23 +66,102 @@ for($i=0; $i < count($inputs);$i++){
 
 
 }
-$label_input=[];
-for($i=0;$i<count($labels);$i++){
-   $label_input[$i]= (object)['label'=>$label[$i],'input'=>$input[$i]];
+
+/* TEXTAREA */
+$textareas = $html->find('p');
+$textarea=[];
+//$input_data=[];
+for($i=0; $i < count($textareas);$i++){
+    $textarea[$i] = (object)[
+    'type' => $textareas[$i]->attr['type'],
+    'class' => $textareas[$i]->attr['class'],
+    'id' => $textareas[$i]->attr['id'],
+    'name' => $textareas[$i]->attr['name'],
+    'cols' => $textareas[$i]->attr['cols'],
+    'rows' => $textareas[$i]->attr['rows'],
+    'placeholder' => $textareas[$i]->attr['placeholder'],
+    'html' => $textareas[$i]->attr['html'],
+    ];
 }
-var_dump($label_input[0]);
-/* LABEL TAGS */
+
+/* END TEXTAREA */
+$label_input=[];
+if(count($labels) > count($input)){
+    for($i=0;$i<count($labels);$i++){
+       $label_input[$i]= (object)['label'=>$label[$i],'input'=>$input[$i],'textarea'=>$textarea[$i]];
+   }
+}else{
+    for($i=0;$i<count($inputs);$i++){
+       $label_input[$i]= (object)['label'=>$label[$i],'input'=>$input[$i],'textarea'=>$textarea[$i]];
+   }
+}
+/* END LABEL & INPUT TAGS */
+
+/* INPUT HIDDEN */
+
+$hiddens = $html->find('input[type=hidden]');
+$hidden = [];
+for($i=0; $i < count($hiddens);$i++){
+
+    $hidden[$i] =(object)[
+    'type' => $hiddens[$i]->attr['type'],
+    'class' => $hiddens[$i]->attr['class'],
+    'name' => $hiddens[$i]->attr['name'],
+    'id' => $hiddens[$i]->attr['id'],
+    'value'=> $hiddens[$i]->attr['value'],
+    ];
+}
+
+/* END INPUT HIDDEN */
+/* INPUT SUBMIT */
+$submits = $html->find('input[type=submit]')[0];
+$submit = [];
+$submit =(object)[
+'type' => $submits->attr['type'],
+'class' => $submits->attr['class'],
+'id' => $submits->attr['id'],
+'value'=> $submits->attr['value'],
+];
+
+/* END INPUT SUBMIT*/
+
+/* END INPUT SUBMIT*/
+/*$i=0;
+foreach($html->find('form',0) as $form){
+    $totalForm[$i] = (object)[
+    'label'=>$label,
+ 'input'=>$input,
+    'textarea'=>$textarea,
+    'hidden'=>$hidden,
+    'submit'=>$submit,
+    ]
+    $i++;
+}*/
+
 if($form_url):?>
 
 <textarea name="" id="" cols="100" rows="100">
     {{Form::open(array('url'=>'<?php echo $form_url;?>','method'=>'<?php echo $form_method; ?>' ))}}
     <?php foreach($label_input as $lb): ?>
-
-        {{Form::<?php echo $lb->label->type;?>('<?php echo $lb->label->for;?>','<?php echo $lb->label->html;?>',array('class'=>'<?php echo $lb->label->class;?>','id'=>'<?php echo $lb->label->id;?>'))}}
-        {{Form::<?php echo $lb->input->type;?>('<?php echo $lb->input->name;?>','<?php echo $lb->input->value;?>',array('class'=>'<?php echo $lb->input->class;?>','id'=>'<?php echo $lb->input->id;?>'))}}
+        <?php if($lb->label): ?>
+            {{Form::<?php echo $lb->label->type;?>('<?php echo $lb->label->for;?>','<?php echo $lb->label->html;?>',array('class'=>'<?php echo $lb->label->class;?>','id'=>'<?php echo $lb->label->id;?>'))}}
+        <?php endif; ?>
+        <?php if($lb->input): ?>
+            {{Form::<?php echo $lb->input->type;?>('<?php echo $lb->input->name;?>','<?php echo $lb->input->value;?>',array('class'=>'<?php echo $lb->input->class;?>','id'=>'<?php echo $lb->input->id;?>'))}}
+        <?php endif; ?>
+        <?php if($lb->textarea): ?>    
+            {{Form::<?php echo $lb->textarea->type;?>('<?php echo $lb->textarea->name;?>','<?php echo $lb->textarea->html;?>',array('class'=>'<?php echo $lb->textarea->class;?>','id'=>'<?php echo $lb->textarea->id;?>'))}}
+        <?php endif; ?>
 
     <?php endforeach; ?>
+    <?php if($hidden): ?>
+        <?php foreach($hidden as $hd): ?>
+            {{Form:<?php echo $hd->type;?>('<?php echo $hd->name;?>','<?php echo $hd->value;?>',array('class'=>'<?php echo $hd->class;?>','id'=>'<?php echo $hd->id;?>'))}}
+        <?php endforeach; ?>
+    <?php endif; ?>
+    {{Form::<?php echo $submit->type;?>('<?php echo $submit->value;?>',array('class'=>'<?php echo $submit->class;?>','id'=>'<?php echo $submit->id;?>'))}}
     {{Form::close()}}
+}
 </textarea> 
 
 <?php elseif($form_route):?>
@@ -130,12 +219,20 @@ if($form_url):?>
                    <form action="index.php" method="post">
 
 
-                    <textarea name="form" id="form" style="min-height:300px; width:600px;margin:auto;display: block"> 
+                    <textarea name="formToLaravel" id="formToLaravel" style="min-height:300px; width:600px;margin:auto;display: block"> 
                         <form method="POST" url="index" param="cours-slug">
                             <label for="intitule" class="toto" id="totid" data-id="9990">Intitulé</label>
                             <input type="text" name="intitule" id="intitule" placeholder="Math" value="val">
                             <label for="distance" class="distance" id="distanceid" data-id="9990">Distance</label>
                             <input type="email" name="distance" id="distanceeee" placeholder="dzdz">
+                            <select name="select" id="selectid">
+                                <option selected value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                            </select>
+                            <p name="toto" type="textarea" id="totoid" class="boudin" cols="30" rows="10">beurk</p>
+                            <input type="hidden" name="data" class="data for the win" value="data:{test:{top:2,lol:4}}">
+                            <input type="submit" class="btn secondary" value="Envoyer">
                         </form>
                     </textarea>
                     <button class="btn send">Envoyer</button>
--- js/main.js
+++ js/main.js
@@ -1,14 +1,9 @@
 ;(function( $ ){
 
 	$(function(){
-		$('.send').on('click',getHTML);
+
 	});
-	var getHTML = function(){
-		sHtml = $('#form').val().replace('\n',"").replace(' ','');
-		
-		//$form = $html.find('<form>');
-		console.log(sHtml.split('<form>'));
-	}
+	
 
-
+//&lt; and &gt;
 }).call(this,jQuery);
